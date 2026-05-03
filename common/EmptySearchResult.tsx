78b2392fb1c1e3469cd53bfba7b334b911bbaa85
Ruslan <rzovliev@gmail.com>
2026-05-03 17:46:51 +0500

    add empty search icon to EmptySearchResult

--- common/EmptySearchResult.tsx
+++ common/EmptySearchResult.tsx
@@ -1,4 +1,5 @@
 import { COLORS } from "@/constants/color.const";
+import { Ionicons } from "@expo/vector-icons";
 import { StyleSheet, View } from "react-native";
 import StyledButton from "../components/StyledButton";
 import StyledText from "../components/StyledText";
@@ -10,17 +11,18 @@ const EmptySearchResult = () => {
 
   return (
     <View style={styles.container}>
+      {/* Круг с иконкой */}
+      <View style={styles.iconContainer}>
+        <Ionicons name="search-outline" size={40} color="#9CA3AF" />
+      </View>
+
       <StyledText style={styles.title}>Ничего не найдено</StyledText>
 
       <StyledText style={styles.subtitle}>
         Попробуйте изменить фильтры или ключевые слова для поиска
       </StyledText>
 
-      <StyledButton
-        onPress={handleAIPick}
-        variant="outline-btn"
-        sizeText="small"
-      >
+      <StyledButton onPress={handleAIPick} variant="outline-btn">
         <StyledText style={styles.buttonText}>
           Посмотреть AI подборку
         </StyledText>
@@ -35,6 +37,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     paddingHorizontal: 60,
+  },
+  iconContainer: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    backgroundColor: COLORS.TAG_BG,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 15,
   },
   title: {
     fontSize: 20,
