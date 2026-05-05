b6bec42bde8b4285ee917565231515c8a52f5680
Ruslan <rzovliev@gmail.com>
2026-05-06 02:57:33 +0500

    add screen ServiceUnavailable

--- layout/Screens/Home/index.tsx
+++ layout/Screens/Home/index.tsx
@@ -1,10 +1,10 @@
-import PaymentRequired from "@/common/ErrorScreen/PaymentRequired";
+import ServiceUnavailable from "@/common/ErrorScreen/ServiceUnavailable";
 
 export default function HomeScreen() {
   const hasResults = false;
 
   if (!hasResults) {
-    return <PaymentRequired />;
+    return <ServiceUnavailable />;
   }
 
   return null;
